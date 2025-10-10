7483ed947dc36fa6cd8c68ef27ab414db934f115
ProudMay <mehdi79nekouei@gmail.com>
2025-10-10 17:09:19 +0330

    Add(scripts & html)
    added main source of project include index.html and scripts.js. also added first prototype and some structure but not working correctly yet :)

---
+++ scripts.js
@@ -0,0 +1,54 @@
+let UChoice = prompt("Inter your choice : \n"+
+                    "R as Rock \n"+
+                    "P as Paper\n"+
+                    "S as Scissors")
+let CRand = Math.floor(Math.random() * 3);
+let MChoice ;
+    if(CRand ==0){
+         MChoice = "Rock"
+    }
+    else if (CRand ==1){
+          MChoice = "Paper"
+        }
+    else{
+          MChoice = "Scissors"
+        }
+    
+let UScore = 0;
+let MScore = 0;
+for (let i=0 ;i<5 ; i++){
+    if(UChoice ==='R' && MChoice === "Rock"){
+        console.log(" Both equal ! ")
+    }
+    else if(UChoice ==='R' && MChoice === "Paper"){
+        MScore++;
+        console.log("Oh, Machine wins this hand !")
+    }
+    else if(UChoice ==='R' && MChoice === "Scissors"){
+        UScore++;
+        console.log("Oh, you win this hand !")
+    }
+    else if(UChoice ==='P' && MChoice === "Paper"){
+        console.log(" Both equal ! ")
+    }
+    else if(UChoice ==='P' && MChoice === "Scissors"){
+        MScore++;
+        console.log("Oh, Machine wins this hand !")
+    }
+    else if(UChoice ==='P' && MChoice === "Rock"){
+        UScore++;
+        console.log("Oh, you win this hand !")
+    }
+    else if(UChoice ==='S' && MChoice === "Rock"){
+        MScore++;
+        console.log("Oh, Machine wins this hand !")
+    }
+    else if(UChoice ==='S' && MChoice === "Paper"){
+        UScore++;
+        console.log("Oh, you win this hand !")
+    }
+    else if(UChoice ==='S' && MChoice === "Scissors"){
+        console.log(" Both equal ! ")
+
+    }
+}
